9ba7987eb475dd91c477b68cae0d23eb60d2e10c
Ahmed Dogan <114875634+MuhammedAhmedDogan@users.noreply.github.com>
2024-03-17 04:57:19 +0300

    TryCatch

--- TryCatch/src/Main.java
+++ TryCatch/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int index(int[] arr, int indexNo) {
+        return arr[indexNo];
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Index Number : ");
+
+        try {
+            int indexNo = input.nextInt();
+            System.out.println(index(arr,indexNo));
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+
     }
 }
